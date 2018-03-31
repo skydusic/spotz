43c5c2e178ef7134165e4f21646269b8326ba6c2
skydusic <skydusic@gmail.com>
2018-03-31 13:12:27 +0900

    업버튼 버그 수정

--- addsup/app/src/main/java/addup/fpcompany/com/addsup/myPageOption.java
+++ addsup/app/src/main/java/addup/fpcompany/com/addsup/myPageOption.java
@@ -1,40 +1,45 @@
 package addup.fpcompany.com.addsup;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import addup.fpcompany.com.addsup.java.BaseActivity;
-
-public class myPageOption extends BaseActivity implements View.OnClickListener {
+public class myPageOption extends Activity implements View.OnClickListener {
 
     TextView upBtn;
     TextView superUpBtn;
+    Typeface mTypeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 상단 바 없애는 코드임
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_my_page_option);
+        mTypeface = Typeface.createFromAsset(getAssets(), "fonts/SDSwaggerTTF.ttf");
 
         upBtn = findViewById(R.id.upBtn);
         superUpBtn = findViewById(R.id.superUpBtn);
+        upBtn.setTypeface(mTypeface);
+        superUpBtn.setTypeface(mTypeface);
 
         upBtn.setOnClickListener(this);
         superUpBtn.setOnClickListener(this);
 
     }
 
-    Intent intent = new Intent();
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case (R.id.upBtn):
-                setResult(100, intent);
+                setResult(100);
                 break;
             case (R.id.superUpBtn):
-                setResult(200, intent);
+                setResult(200);
                 break;
         }
         finish();
